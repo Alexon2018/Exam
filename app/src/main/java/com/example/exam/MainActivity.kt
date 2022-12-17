8a3050831d723cff5cd051157ecf8415aba7f105
Alexon2018 <alex.motz@gmail.com>
2022-12-17 21:19:08 +0200

    completat navigarea pe fragmente, meniu, drawer, UP

--- app/src/main/java/com/example/exam/MainActivity.kt
+++ app/src/main/java/com/example/exam/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.exam
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.exam.databinding.ActivityMainBinding
 import com.example.exam.dataclasses.MyItem
@@ -11,27 +15,31 @@ import com.example.exam.utils.MyAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private val filteredArray = ArrayList<MyItem>()
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        drawerLayout = binding.drawerLayout
 
-//mock data
-        filteredArray.clear()
-        var temp = MyItem(
-            "Care vehicul va trece ultimul prin intersectie?",
-            Response("Tramvaiul", false),
-            Response("Ambulanta cu semnalalele luminoase si sonore pornite", false),
-            Response("BMW-ul cu numere de Bulgaria", true),
-            "Categoria intai",
-            "23-12-2022"
-        )
-        filteredArray+=temp
-//end of mock data
+//suport pentru UP button
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+////mock data
+//        filteredArray.clear()
+//        var temp = MyItem(
+//            "Care vehicul va trece ultimul prin intersectie?",
+//            Response("Tramvaiul", false),
+//            Response("Ambulanta cu semnalalele luminoase si sonore pornite", false),
+//            Response("BMW-ul cu numere de Bulgaria", true),
+//            "Categoria intai",
+//            "23-12-2022"
+//        )
+//        filteredArray+=temp
+////end of mock data
 
 //        binding.rv.layoutManager = LinearLayoutManager(this)
 //        binding.rv.adapter= MyAdapter(filteredArray)
@@ -43,4 +51,12 @@ class MainActivity : AppCompatActivity() {
 //        }
 
     }
+
+    //tot pentru UP button
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+
 }
--- app/src/main/java/com/example/exam/allfragments/rvFragment.kt
+++ app/src/main/java/com/example/exam/allfragments/rvFragment.kt
@@ -1,12 +1,11 @@
 package com.example.exam.allfragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.exam.R
 import com.example.exam.databinding.FragmentRvBinding
 
@@ -23,5 +22,17 @@ class rvFragment : Fragment() {
         setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
 
 }
