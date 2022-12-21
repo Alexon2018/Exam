923d7a6c586f464999b24c5edfed652b6778cbd4
Alexon2018 <alex.motz@gmail.com>
2022-12-21 17:20:03 +0200

    glide about contact call share

--- app/src/main/java/com/example/exam/Views/allfragments/ContactFragment.kt
+++ app/src/main/java/com/example/exam/Views/allfragments/ContactFragment.kt
@@ -1,12 +1,20 @@
 package com.example.exam.Views.allfragments
 
+import android.Manifest.permission.CALL_PHONE
+import android.app.Application
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.example.exam.ItemListFragmentDirections
 import com.example.exam.R
 import com.example.exam.databinding.FragmentAddItemBinding
 import com.example.exam.databinding.FragmentContactBinding
@@ -29,6 +37,15 @@ class ContactFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.callButton.setOnClickListener { makePhoneCall(this.resources.getString(R.string.phoneNumber)) }
+    }
+
+    private fun makePhoneCall(number:String) {
+        val intent = Intent()
+        intent.action = Intent.ACTION_DIAL
+        intent.data = Uri.parse("tel: $number")
+        startActivity(intent)
     }
 
 
