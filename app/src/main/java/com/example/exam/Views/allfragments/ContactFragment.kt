20701c22eb1c53eabe6cecdd09f36fa667ef2e5c
Alexon2018 <alex.motz@gmail.com>
2022-12-21 13:19:35 +0200

    share feature

---
+++ app/src/main/java/com/example/exam/Views/allfragments/ContactFragment.kt
@@ -0,0 +1,50 @@
+package com.example.exam.Views.allfragments
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.exam.R
+import com.example.exam.databinding.FragmentAddItemBinding
+import com.example.exam.databinding.FragmentContactBinding
+import com.example.exam.utils.Context
+
+
+class ContactFragment : Fragment() {
+
+    private var _binding: FragmentContactBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentContactBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.shareButton.setOnClickListener {
+            startSharing()
+        }
+
+    }
+
+    private fun startSharing(){
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Iti trimit cea mai cool aplicatie")
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(sendIntent,null)
+        shareIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        this.startActivity(shareIntent)
+    }
+
+}
